446b3747ece41addcf73dfe758b3905132bf51b4
quillenink <36284902+sushitoad@users.noreply.github.com>
2024-03-02 21:10:14 -0800

    finishing 45

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,10 +1,5 @@
 extends Node2D
 
-var enemies_escaped = 0
 
 func _on_deathzone_area_entered(area):
-	#If area.collision_layer = 2:
-	#	print("enemy hit deathzone")
 	area.die()
-	enemies_escaped += 1
-	print(enemies_escaped)
