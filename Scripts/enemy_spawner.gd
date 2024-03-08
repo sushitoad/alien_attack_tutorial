a3402dee45d6ed9b5548c5cf97d60cf2b06c87f4
quillenink <36284902+sushitoad@users.noreply.github.com>
2024-03-07 19:44:15 -0800

    finish!

--- Scripts/enemy_spawner.gd
+++ Scripts/enemy_spawner.gd
@@ -1,8 +1,10 @@
 extends Node
 
 signal enemy_spawned(enemy_instance)
+signal path_enemy_spawned(path_enemy_instance)
 
 var enemy_scene = preload("res://Scenes/enemy.tscn")
+var path_enemy_scene = preload("res://Scenes/path_enemy.tscn")
 @onready var spawn_positions = $SpawnPositions
 
 func spawn_enemy():
@@ -11,6 +13,13 @@ func spawn_enemy():
 	var new_spawn_pos = pos_array.pick_random()
 	new_enemy.global_position = new_spawn_pos.global_position
 	emit_signal("enemy_spawned", new_enemy)
-	
+
 func _on_timer_timeout():
 	spawn_enemy()
+
+func spawn_path_enemy():
+	var path_enemy = path_enemy_scene.instantiate()
+	emit_signal("path_enemy_spawned", path_enemy)
+
+func _on_path_enemy_timer_timeout():
+	spawn_path_enemy()
